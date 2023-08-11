0e1e701194a299bd2ffa6e27784b2f75f39fc89e
Edgar Bañuelos Estrada <etxu00@gmail.com>
2023-08-11 00:35:16 -0600

    feat: :sparkles: MVP
    Proyecto jugable

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 
 // Elementos del DOM
-const $buttons = document.querySelectorAll('button');
+const
+  $buttons = document.querySelectorAll('button'),
+  $body   = document.querySelector('body');
+  $main   = document.querySelector('main');
+
 
 // Asignación de eventos
 $buttons.forEach(($button) => {
@@ -9,22 +13,27 @@ $buttons.forEach(($button) => {
 
 // Funciones
 const letsGo = event => {
+  $body.classList.remove('win', 'lose', 'draw', '___Piedra', '___Papel', '___Tijera');
+  $main.classList.remove('___Piedra', '___Papel', '___Tijera');
+  $buttons.forEach($button => $button.disabled = true);
   const optionUser = event.target.textContent;
   play(optionUser);
+  setTimeout(() => $buttons.forEach($button => $button.disabled = false), 1000);
 }
 const play = optionUser => {
   const
     options           = ['Piedra', 'Papel', 'Tijera'],
     optionComputer    = options[Math.floor(Math.random() * 3)],
     result            = getResult(optionUser, optionComputer);
-  alert(`Tu elegiste ${optionUser} y la computadora ${optionComputer}.\n${result}`);
+  $body.classList.add(result, `___${optionComputer}`);
+  //atributo role para main con el valor de optionUser
+  $main.classList.add(`___${optionUser}`);
 }
 const getResult = (user, pc) => {
   const options = {
     'Piedra'    : () => result = pc === 'Tijera' ? 'win' : 'lose',
     'Papel'     : () => result = pc === 'Piedra' ? 'win' : 'lose',
-    'Tijera'    : () => result = pc === 'Papel' ? 'win' : 'lose',
-    'default'   : () => result = 'draw'
+    'Tijera'    : () => result = pc === 'Papel' ? 'win' : 'lose'
   }
-  return (options[user] || options['default'])();
+  return user === pc ? 'draw' : options[user]();
 }
